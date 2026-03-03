4f8748858112732ed83ef7723118ef571d8a9ece
nathan-wallace-hhs <nathan.wallace@hhs.gov>
2026-03-03 08:02:39 -0500

    Create .storybook/preview.js for Storybook configuration

---
+++ .storybook/preview.js
@@ -0,0 +1,12 @@
+// .storybook/preview.js
+
+// Configure Storybook's preview settings here
+
+import { addParameters } from '@storybook/react';
+
+addParameters({
+  options: {
+    showPanel: true,
+    panelPosition: 'bottom',
+  },
+});
